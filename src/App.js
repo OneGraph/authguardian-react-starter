5829200700ecac9ed380947f208a30d36321fa66
Sean Grove <sean@bushi.do>
2020-04-29 18:08:50 -0700

    Prettier

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import OneGraphAuth from "onegraph-auth";
+import fetchSupportedServicesQuery from "./fetchSupportedServices.js";
 import "./App.css";
 
-const appId = process.env.ONE_GRAPH_APP_ID;
+const appId = process.env.REACT_APP_ONE_GRAPH_APP_ID;
 
 const auth = new OneGraphAuth({
   appId: appId,
@@ -12,55 +13,6 @@ const auth = new OneGraphAuth({
 const gitHubLink =
   process.env.GITHUB_URL ||
   "https://github.com/OneGraph/authguardian-react-starter";
-
-// This setup is only needed once per application
-async function fetchOneGraph(operationsDoc, operationName, variables) {
-  const result = await fetch(
-    `https://serve.onegraph.com/graphql?app_id=${appId}`,
-    {
-      method: "POST",
-      headers: {
-        ...auth.authHeaders(),
-      },
-      body: JSON.stringify({
-        query: operationsDoc,
-        variables: variables,
-        operationName: operationName,
-      }),
-    }
-  );
-
-  return await result.json();
-}
-
-const operationsDoc = `
-  query SupportedServicesQuery {
-    oneGraph {
-      services {
-        service
-        friendlyServiceName
-        slug
-        supportsOauthLogin
-        supportsCustomServiceAuth
-      }
-    }
-  }
-`;
-
-async function fetchSupportedServicesQuery() {
-  const result = await fetchOneGraph(
-    operationsDoc,
-    "SupportedServicesQuery",
-    {}
-  );
-
-  const oauthServices = result.data.oneGraph && result.data.oneGraph.services;
-  const supportedServices = oauthServices
-    .filter((service) => service.supportsOauthLogin)
-    .sort((a, b) => a.friendlyServiceName.localeCompare(b.friendlyServiceName));
-
-  return supportedServices;
-}
 
 const exampleUsage = (appId, service) => {
   return `import React, { useState, useEffect } from "react";
@@ -116,6 +68,22 @@ function corsPrompt(appId) {
   );
 }
 
+const gitHubIcon = (
+  <svg
+    role="img"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    width="20px"
+    height="20px"
+  >
+    <title>GitHub icon</title>
+    <path
+      fill="white"
+      d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+    />
+  </svg>
+);
+
 function navBar(appId) {
   return (
     <nav>
@@ -127,7 +95,7 @@ function navBar(appId) {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Edit your rules
+            Edit your rules {JSON.stringify(process.env, null, 2)}?
           </a>
         </li>
         <li>
@@ -142,19 +110,7 @@ function navBar(appId) {
         </li>
         <li>
           <a href={gitHubLink} target="_blank" rel="noopener noreferrer">
-            <svg
-              role="img"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              width="20px"
-              height="20px"
-            >
-              <title>GitHub icon</title>
-              <path
-                fill="white"
-                d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
-              />
-            </svg>
+            {gitHubIcon}
           </a>
         </li>
       </ul>
